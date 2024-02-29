a591959ad040b036e3fa1d7d108c12fc673fd027
samuelpack <sampack@me.com>
2024-02-29 11:23:45 -0500

    first commit

---
+++ app/routes.py
@@ -0,0 +1,44 @@
+from flask import render_template, flash, redirect, url_for
+from app import app
+from app.forms import LoginForm
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Sam'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Sam'},
+            'body': 'I dont want to do this anymore!'
+        },
+        {
+            'author': {'username': 'Shawn'},
+            'body': 'Respect my authority!'
+        },
+        {
+            'author': {'username': 'Richard'},
+            'body': 'Dont call it a comeback!'
+        },
+        {
+            'author': {'username': 'Arnold'},
+            'body': 'What are you talking about, Willis?'
+        },
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('/index'))
+    return render_template('login.html', title='Sign In', form=form)
+
